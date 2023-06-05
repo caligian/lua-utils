5af7894b9889024ebb57a4fece37c03cb0d37fac
Krunal Pamkar <caligian@protonmail.com>
2023-06-05 10:30:48 +0530

    first commit

---
+++ init.lua
@@ -0,0 +1,22 @@
+array = require 'lua-utils.array'
+class = require 'lua-utils.class'
+dict = require 'lua-utils.dict'
+exception = require 'lua-utils.exception'
+fn = require 'lua-utils.fn'
+multimethod = require 'lua-utils.multimethod'
+param = require 'lua-utils.param'
+pprint = require 'lua-utils.pprint'
+Set = require 'lua-utils.Set'
+str = require 'lua-utils.str'
+types = require 'lua-utils.types'
+utils = require 'lua-utils.utils'
+
+local function makeglobal(t)
+  for key, value in pairs(t) do _G[key] = value end
+end
+
+makeglobal(types)
+makeglobal(utils)
+makeglobal(fn)
+makeglobal(pprint)
+makeglobal(param)
