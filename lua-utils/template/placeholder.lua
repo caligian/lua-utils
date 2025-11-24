67d924c2ddc6ff26bb39264d7584e72242e6cb4e
caligian <caligian@protonmail.com>
2025-11-24 18:55:20 +0530

    Fix: remove unnecessary validation

--- lua-utils/template/placeholder.lua
+++ lua-utils/template/placeholder.lua
@@ -3,6 +3,27 @@ require 'lua-utils.utils'
 
 ---String matching utilities for template strings
 local placeholder = {}
+local pyproject_toml = [[[project]
+name = "{name}"
+version = "0.0.1"
+authors = [
+  \\{ name="{author}", email="{email}" \\},
+]
+description = "A small example package"
+readme = "README.md"
+requires-python = ">={version}"
+classifiers = [
+  "Programming Language :: Python :: 3",
+  "Operating System :: OS Independent",
+]
+license = "{license}"
+license-files = ["LICEN[CS]E*"]
+
+[project.urls]
+Homepage = "https://github.com/{username}/{name}"
+Issues = "https://github.com/{username}/{name}/issues"
+]]
+
 
 ---Check if placeholder name is valid
 ---@param name string | number
@@ -15,108 +36,50 @@ function placeholder.check_name(name)
   end
 end
 
----Find the next opening or closing bracket
----@param s string
----@param start number (default: 1) Start index
----@param is_closing? boolean Whether to match '}' instead of '{'
----@return number?
-function placeholder.find_next_bracket(s, start, is_closing)
-  local open = not is_closing
-  local bracket = open and '{' or '}'
+function placeholder.find_next_pair(s, start)
   start = start or 1
-  local l = #s
-  local i = start
+  local opening = string.find(s, '{', start, true)
 
-  while i <= l do
-    local c = s:sub(i, i)
-
-    if c == bracket then
-      local pos = i
-      local till = string.sub(s, start, pos - 1)
-      local escaped = string.match(till, '\\+$')
-
-      if not escaped then
-        return pos
-      elseif #escaped % 2 == 0 then
-        return pos
-      else
-        start = pos + 1
-        i = pos + 1
-      end
-    else
-      i = i + 1
-    end
-  end
-end
-
----Find '{'
----@param s string
----@param start? number Start index
----@return number?
-function placeholder.find_opening_bracket(s, start)
-  return placeholder.find_next_bracket(s, start, false)
-end
-
-placeholder.opening = placeholder.find_opening_bracket
-
----Find '}'
----@param s string
----@param start? number Start index
----@return number?
-function placeholder.find_closing_bracket(s, start)
-  return placeholder.find_next_bracket(s, start, true)
-end
-
-placeholder.closing = placeholder.find_closing_bracket
-
----Return the position of next '{' and '}' in string 
----@param s string
----@param start? number
----@return number[]?
-function placeholder.find_next_brackets(s, start)
-  local open_pos = placeholder.opening(s, start)
-  local close_pos = placeholder.closing(s,
-    open_pos and open_pos + 1 or start
-  )
-
-  if open_pos and close_pos then
-    if open_pos > close_pos then
-      errorf('No opening bracket given for closing bracket at char %d in `%s`', open_pos, s)
-    else
-      return { open_pos, close_pos }
-    end
-  elseif not open_pos and not close_pos then
+  if not opening then
     return
-  elseif not open_pos then
-    errorf('No opening bracket given for closing bracket at char %d in `%s`', close_pos, s)
-  elseif not close_pos then
-    errorf('No closing bracket given for opening bracket at char %d in `%s`', open_pos, s)
   end
+
+  local till_opening = string.sub(s, start, opening - 1)
+  local _, escaped = till_opening:gsub('\\+$', '')
+
+  if not (escaped == 0 or escaped % 2 == 0) then
+    return placeholder.find_next_pair(s, opening + 1)
+  end
+
+  local closing = s:find('\\*[}]', opening + 1)
+  if not closing then
+    error(sprintf("Could not find closing bracket for opening parenthesis on character %d", opening))
+  end
+
+  _, escaped = till_opening:gsub('\\+$', '')
+  if not (escaped == 0 or escaped % 2 == 0) then
+    return placeholder.find_next_pair(s, till_opening + 1)
+  end
+
+  return opening, closing
 end
 
-placeholder.pair = placeholder.find_next_brackets
-
----@class placeholder.next.result
----@field before string
----@field placeholder string | number
----@field next number
----@field string string
+placeholder.next_pair = placeholder.find_next_pair
 
 ---Parse one placeholder and return the next state
 ---@param s string
 ---@return placeholder.next.result?
-function placeholder.next(s, start)
-  local pos = placeholder.pair(s, start)
+function placeholder.find_next(s, start)
+  start = start or 1
+  local open_pos, close_pos = placeholder.next_pair(s, start)
 
-  if not pos then
+  if not open_pos then
     return {
       string = s,
       next = false
     }
   end
 
-  start = start or 1
-  local open_pos, close_pos = unpack(pos)
   local name = s:sub(open_pos + 1, close_pos - 1)
   placeholder.check_name(name)
   local before = s:sub(start, open_pos - 1)
@@ -136,5 +99,7 @@ function placeholder.next(s, start)
 
   return result
 end
+
+placeholder.next = placeholder.find_next
 
 return placeholder
