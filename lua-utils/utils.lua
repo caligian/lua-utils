4b5bd763ace67b719e812b53d2c14dfe04f9b380
caligian <caligian@protonmail.com>
2026-05-22 23:34:55 +0530

    refactor a lot of nonsense to reflect golang like ok, msg_value return type

--- lua-utils/utils.lua
+++ lua-utils/utils.lua
@@ -1,33 +1,102 @@
-inspect = require 'lua-utils.inspect'
+local _ = require 'lua-utils._'
+local metatable = require 'lua-utils.metatable'
 local tuple = require('lua-utils.tuple')
-local list = require('lua-utils.list')
+
 unpack = unpack or table.unpack
+inspect = require 'lua-utils.inspect'
 
+---@alias Filter fun(...): boolean?
+---@alias Mapper fun(...): any
+---@alias Condition boolean | Filter | nil
+---@alias OkCallback fun(): any
+---@alias ErrCallback fun(): any
 
----@alias Condition (fun(...): boolean) | boolean | nil
----@alias Mapper (fun(...): any)
-
----@param cond Condition
-local function run_cond(cond, ...)
-  local cond_type = type(cond)
-  if cond_type == 'boolean' then
-    return cond
-  elseif cond_type == 'function' then
-    return cond(...)
-  elseif cond == nil then
-    return false
+---Anything other than nil and false are truthy values.
+---No more 0 and 1 shenanigans
+---@param x any
+---@return boolean
+function is_truthy(x)
+  if x ~= nil then
+    return true
+  elseif x ~= false then
+    return true
   else
-    error('cond: Expected fun(): bool | boolean, got ' .. (cond_type == nil and 'nil' or cond_type))
+    return false
   end
 end
 
----@param tbl? table
----@return table
-function refself(tbl)
-  tbl = tbl or {}
-  tbl.__index = tbl
+---nil and false are falsy values
+---No more 0 and 1 shenanigans
+---@param x any
+---@return boolean
+function is_falsy(x)
+  if x == nil then
+    return true
+  elseif x == false then
+    return true
+  else
+    return false
+  end
+end
 
-  return setmetatable(tbl, tbl)
+---@param x any
+---@param f fun(...): any
+---@param ... any
+---@return boolean, any
+function when_truthy(x, f, ...)
+  if x ~= nil then
+    return true, f(...)
+  elseif x ~= false then
+    return true, f(...)
+  else
+    return false, nil
+  end
+end
+
+---@param x any
+---@param f fun(...): any
+---@param ... any
+---@return boolean, any
+function when_falsy(x, f, ...)
+  if x == nil then
+    return true, f(...)
+  elseif x == false then
+    return true, f(...)
+  else
+    return false, nil
+  end
+end
+
+---Similar to python's callable()
+---@param x any
+---@param err_msg? boolean
+---@return boolean, string?
+---@overload fun(x: any): boolean, nil
+---@overload fun(x: any, err_msg: boolean): boolean, string
+function callable(x, err_msg)
+  local type_ = type(x)
+  if type_ == 'function' then
+    return true, nil
+  elseif type_ ~= 'table' then
+    if err_msg then
+      return false, 'x: Expected with table with __call metamethod'
+    else
+      return false, nil
+    end
+  end
+
+  local mt = getmetatable(x)
+  if not mt then
+    if err_msg then
+      return false, 'x: Expected table with metamethod'
+    else
+      return false, nil
+    end
+  elseif mt.__call then
+    return callable(mt.__call)
+  else
+    return false, "x: No __call metamethod defined"
+  end
 end
 
 --- Dump object as string
@@ -35,7 +104,7 @@ end
 ---@return string
 function dump(x)
   if x == nil then
-    return 'NIL'
+    return 'nil'
   elseif type(x) == 'string' then
     return x
   elseif type(x) == 'number' then
@@ -53,63 +122,68 @@ function dump(x)
   end
 end
 
----Is value a callable (function or table with .call metamethod)
----@param x any
----@return boolean, string?
-function callable(x)
-  if x == nil then
-    return false, 'expected function|callable, got nothing'
-  elseif not types.fun(x) and not types.table(x) then
-    return false, sprintf('expected function | callable, got %s', x)
-  elseif types.fun(x) then
-    return true
-  elseif types.table(x) then
-    local mt = getmetatable(x)
-    if mt and mt.__call then
-      return types.callable(mt.__call)
-    else
-      return false, sprintf('expected table with __call, got %s', x)
-    end
-  else
-    return false, 'expected function|callable, got ' .. type(x)
-  end
-end
-
 ---@param value any
----@param skip_fn? boolean (default: true)
-function literal(value, skip_fn)
-  if type(value) == 'function' and skip_fn then
-    return value
-  else
-    return function()
-      return value
-    end
-  end
+---@return (fun(): any)
+function literal(value)
+  return function() return value end
 end
 
----Inverse of ifelse
+---Inverse functional version of if-else
 ---@param cond boolean
 ---@param ok any
 ---@param err? any
 ---@return any
----@overload fun(x: any): boolean
-function unless(cond, err, ok)
-  cond = (type(cond) == 'boolean' and cond) or function(x)
-    return not cond(x)
+function unless(cond, ok, err)
+  local use
+  if callable(cond) then
+    use = cond()
+  else
+    use = cond
   end
-  return ifelse(cond, err, ok)
+
+  return when(not use, ok, err)
+end
+
+---Functional version of if-else
+---@param cond Condition
+---@param ok? OkCallback
+---@param err? ErrCallback
+---@return any
+---@overload fun(cond: Condition): boolean
+function when(cond, ok, err)
+  local use
+  if callable(cond) then
+    use = cond()
+  else
+    use = cond
+  end
+
+  if use then
+    if ok then
+      return ok()
+    else
+      return true
+    end
+  elseif err then
+    return err()
+  else
+    return false
+  end
 end
 
 ---Call a function with arguments (with pcall optionally)
+---Do not return more than 2 arguments, variadic returns other than 2 can cause chaos
 ---@param f function
 ---@param args any[]
 ---@param should_pcall? boolean (default: false)
----@return any, string?
+---@return boolean, string?
+---@overload fun(f: function, args: []any, should_pcall: true): boolean, any
+---@overload fun(f: function, args: []any, should_pcall: false): any
 function apply(f, args, should_pcall)
   if should_pcall then
     local ok, msg = pcall(f, unpack(args))
     if ok then
-      return msg
+      return true, msg
     else
       return false, msg
     end
@@ -119,30 +193,42 @@ function apply(f, args, should_pcall)
 end
 
 ---Curry functions with arguments at the beginning of the function call
+---@param default any non-nil value
 ---@param f function
----@param ... any
+---@param ... any Outer function arguments
 ---@return function
-function partial(f, ...)
-  local args = tuple.pack(...)
+function partial(default, f, ...)
+  local outer = tuple.pack(default, ...)
   return function(...)
-    local inner = tuple.pack(...)
-    args = { unpack(args) }
-    list.extend(args, inner)
-    return f(unpack(args))
+    local inner = tuple.pack(default, ...)
+    local len = #outer
+
+    for i=1, #inner do
+      outer[len+1] = inner[i]
+      len = len + 1
+    end
+
+    return f(unpack(outer))
   end
 end
 
----Curry functions with arguments at the end of the function call
+---Curry functions with arguments at the beginning of the function call
+---@param default any non-nil value
 ---@param f function
----@param ... any
+---@param ... any Outer function arguments
 ---@return function
-function rpartial(f, ...)
-  local args = tuple.pack(...)
+function rpartial(default, f, ...)
+  local outer = tuple.pack(default, ...)
   return function(...)
-    local inner = tuple.pack(...)
-    args = { unpack(args) }
-    args = list.extend(inner, args)
-    return f(unpack(args))
+    local inner = tuple.pack(default, ...)
+    local len = #inner
+
+    for i=1, #outer do
+      inner[len+1] = outer[i]
+      len = len + 1
+    end
+
+    return f(unpack(inner))
   end
 end
 
@@ -151,28 +237,27 @@ end
 ---@param ... any
 ---@return string
 function sprintf(fmt, ...)
-  local args = tuple.pack(...)
+  local args = tuple.pack('`nil`', ...)
 
   for i = 1, #args do
     local x = args[i]
     local _type = type(x)
 
     if _type == 'table' then
-      if x.as_string then
-        args[i] = x:as_string()
-      elseif x.__tostring then
-        args[i] = x:__tostring()
+      local mt = getmetatable(x)
+      if mt and mt.__tostring then
+        args[i] = mt.__tostring(x)
       else
         args[i] = dump(x)
       end
     elseif _type == 'string' and _type == 'number' then
       args[i] = tostring(args[i])
     else
-      args[i] = dump(args[i])
+      args[i] = inspect(args[i])
     end
   end
 
-  return apply(string.format, list.extend({ fmt }, args))
+  return string.format(fmt, unpack(args))
 end
 
 ---Same as sprintf but print the string
@@ -182,38 +267,80 @@ function printf(fmt, ...)
   print(sprintf(fmt, ...))
 end
 
----Poor man's thread operator
----Thread the object and its results into consecutive functions
----Use [l]partial in conjunction with this function to actually make this function useful
----@param obj any
----@param ... function
-function thread(obj, ...)
-  local res = obj
-  local map = tuple.pack(...)
-
-  if #map == 0 then
-    return res
-  end
-
-  res = { map[1](res) }
-  for i = 2, #map do
-    res = { map[i](unpack(res)) }
-  end
-
-  return unpack(res)
+---Return argument as is
+---@param x any
+---@return any
+function identity(x)
+  return x
 end
 
----Return arguments as is
----@param ... any
----@return ...
-function identity(...)
-  return ...
+---@overload fun(obj: any, ...: (fun(x: any): boolean, any)): boolean, any
+thread = {}
+thread.__index = thread
+setmetatable(thread, thread)
+
+function thread:__call(obj, ...)
+  local res = obj
+  local mappers = {...}
+
+  if #mappers == 0 then
+    return true, res
+  end
+
+  for i=1, #mappers do
+    local result = {mappers[i](res)}
+    local result_len = #result
+    local ok, msg
+
+    if result_len > 2 then
+      errorf("%s: Expected <= 2 return values, got %d", tostring(mappers[i]), result_len)
+    elseif #result == 1 then
+      ok = is_truthy(result[1])
+      msg = ((not ok) and sprintf('%s: Function returned a falsy value', tostring(result[1]))) or result[1]
+    else
+      ok, msg = unpack(result)
+    end
+
+    if not ok then
+      return false, msg
+    else
+      res = msg
+    end
+  end
+
+  return true, res
+end
+
+---Use pcall instead of returning (false, string?) | (true, {value})
+---@param obj any
+---@param ... fun(obj: any): any
+---@return boolean, any
+function thread.pcall(obj, ...)
+  local res = obj
+  local mappers = {...}
+
+  if #mappers == 0 then
+    return true, res
+  end
+
+  for i=1, #mappers do
+    local ok, msg = pcall(mappers[i], res)
+    if not ok then
+      return false, msg
+    else
+      res = msg
+    end
+  end
+
+  return true, res
 end
 
 ---Print a list of dumped values
+---Do not mistake this with sprintf
+---@param default non-nil value
 ---@param ... any
-function pp(...)
-  local args = tuple.pack(...)
+function pp(default, ...)
+  local args = tuple.pack(default, ...)
   if #args == 0 then
     return
   else
@@ -221,39 +348,19 @@ function pp(...)
   end
 end
 
----Similar to R's paste0 with automatic dumping of arguments
----@param ... any
+---Similar to R's paste0
+---@param ... string 
 ---@return string
 function paste0(...)
-  local args = tuple.pack(...)
-  for i = 1, #args do args[i] = dump(args[i]) end
-  return table.concat(args, '')
+  return table.concat({...}, '')
 end
 
----Similar to R's paste with automatic dumping of arguments
----This function automatically flattens a nested list with depth 1
----@param collapse string Use this to join the dumped arguments
----@param ... any
+---Similar to R's paste0
+---@param sep? string (default: ' ')
+---@param ... string 
 ---@return string
-function paste(collapse, ...)
-  collapse = collapse or ' '
-  local args = tuple.pack(...)
-  local s = {}
-  local ind = 0
-
-  for i = 1, #args do
-    if type(args[i]) == 'table' then
-      for j = 1, #args[i] do
-        s[ind + 1] = dump(args[i][j])
-        ind = ind + 1
-      end
-    elseif type(args[i]) == 'string' then
-      s[ind + 1] = dump(args[i])
-      ind = ind + 1
-    end
-  end
-
-  return table.concat(s, collapse)
+function paste(sep, ...)
+  return table.concat({...}, sep or ' ')
 end
 
 ---Similar to python's callable. Check if object is callable
@@ -291,13 +398,14 @@ function as_list(x, force)
   end
 end
 
-totable = as_list
-
 ---Read a file
 ---@param filename string
+---@param chomp? boolean (default: true)
 ---@return string?
-function slurp(filename)
+function slurp(filename, chomp)
+  chomp = (chomp == nil and true) or chomp
   local fh = io.open(filename, 'r')
+
   if not fh then
     return
   end
@@ -305,23 +413,29 @@ function slurp(filename)
   local text = fh:read('*all')
   fh:close()
 
-  return text
+  return (chomp and string.gsub(text, "\n$", "")) or text
 end
 
 ---Write text to a file
+---@param s string
 ---@param filename string
+---@param newline? boolean (default: true)
 ---@return number?
-function spit(s, filename)
+function spit(s, filename, newline)
+  newline = (newline == nil and true) or false
   local fh = io.open(filename, 'w')
+
   if not fh then
     return
   end
 
+  s = newline and (s .. "\n") or s
+  local len = #s
+
   fh:write(s)
-  fh:write("\n")
   fh:close()
 
-  return #s
+  return len
 end
 
 ---Similar to slurp but returns list of strings
@@ -342,25 +456,33 @@ function readlines(filename)
 end
 
 ---Similar to spit but writes list of strings separated by newline
+---@param lines []string
 ---@param filename string
+---@param newline? boolean (default: true) Add newline at the end of every line? 
 ---@return number?
-function writelines(lines, filename)
+function writelines(lines, filename, newline)
+  newline = (newline == nil and true) or newline
   local fh = io.open(filename, 'w')
+
   if not fh then
     return
   end
 
   local size = 0
-  local l = #lines
+  local len = #lines
 
-  for i = 1, l do
+  for i = 1, len do
     fh:write(lines[i])
-    fh:write("\n")
-    size = #lines[i] + 1
+    size = #lines[i]
+
+    if newline then
+      fh:write("\n")
+      size = size + 1
+    end
   end
 
   fh:close()
-  return size + #lines[l]
+  return size
 end
 
 ---Error messages on steroids - error + sprintf
@@ -374,100 +496,28 @@ end
 ---@param cond boolean Condition to test
 ---@param msg string Message format or message
 ---@param ... any Rest arguments to pass to sprintf
+---@return boolean?
 function assertf(cond, msg, ...)
   if not cond then
     errorf(msg, ...)
+  else
+    return true
   end
-end
-
----Metatable management utilities
----@overload fun(x: table, key?: string | number): any
----@overload fun(x: table): table?
-metatable = refself {}
-
----Check if a metatable field exists
----@param x table
----@return boolean
-function metatable.exists(x)
-  return getmetatable(x) ~= nil
-end
-
----Get a metatable or metatable field
----@param x table
----@return table?
-function metatable.get(x)
-  return getmetatable(x)
-end
-
----Get a metatable or metatable field
----@param x table
----@param key string
----@return table?
-function metatable.get_key(x, key)
-  local mt = getmetatable(x)
-  if mt then
-    return mt[key]
-  end
-end
-
----@param x table
----@param ks []string
----@return table<string,any>?
-function metatable.get_keys(x, ks)
-  local mt = getmetatable(x)
-  if mt == nil then
-    return nil
-  end
-
-  local res = {}
-  for k, v in pairs(mt) do
-    res[k] = v
-  end
-
-  return res
-end
-
----Set a metatable or metatable field
----@param x table
----@param mt table
----@return table
-function metatable.set(x, mt)
-  return setmetatable(x, mt)
-end
-
----Set a metatable or metatable field
----@param x table
----@param key string
----@param value any
----@return table
-function metatable.set_key(x, key, value)
-  local mt = metatable.get(x) or {}
-  mt[key] = value
-
-  return setmetatable(x, mt)
-end
-
----@param x table
----@param keys_and_values table<string,any>
----@return table
-function metatable.set_keys(x, keys_and_values)
-  local mt = getmetatable(x) or {}
-  for key, value in pairs(keys_and_values) do
-    mt[key] = value
-  end
-
-  return setmetatable(x, mt)
 end
 
 ---@param x any
----@param ok? (fun(x): any)
----@param ... any Arguments to pass when x is defined and cond is a function
+---@param ok? (fun(...): any)
 ---@return any
 ---@overload fun(x: any): boolean
+---@overload fun(x: any, ok: (fun(...): any), ...): any
 function undefined(x, ok, ...)
   if x == nil then
     if ok ~= nil then
-      return run_cond(ok, ...)
+      if callable(ok) then
+        return ok(...)
+      else
+        return true
+      end
     else
       return true
     end
@@ -481,10 +531,11 @@ end
 ---@param ... any Arguments to pass when x is defined and cond is a function
 ---@return any
 ---@overload fun(x: any): boolean
+---@overload fun(x: any, ok: (fun(...): any), ...): any
 function defined(x, ok, ...)
   if x ~= nil then
     if ok ~= nil then
-      return run_cond(ok, ...)
+      return _.cond(ok, ...)
     else
       return true
     end
@@ -496,7 +547,8 @@ end
 ---@class ifelse_opts
 ---@field ok? []any
 ---@field err? []any
----@field cond? []any
+---@field ok_args? []any
+---@field err_args? []any
 
 ---@param cond? Condition
 ---@param true_fn? (fun(x): any)
@@ -504,23 +556,11 @@ end
 ---@param opts? ifelse_opts
 ---@return any
 ---@overload fun(x: any): boolean
-function ifelse(cond, true_fn, false_fn, opts)
+function when(cond, true_fn, false_fn, opts)
   opts = opts or {}
-  local ok_args = opts.ok or {}
-  local err_args = opts.err or {}
-  local cond_args = opts.cond or {}
-  local cond_type = type(cond)
-  local cond_fn = cond_type == 'function'
-  local cond_bool = cond_type == 'boolean'
-  local ok = false
-
-  if cond_fn then
-    ok = cond_fn(unpack(cond_args))
-  elseif cond_bool then
-    ok = cond_bool
-  elseif cond == nil then
-    ok = false
-  end
+  local ok_args = opts.ok or opts.ok_args or {}
+  local err_args = opts.err or opts.err_args or {}
+  local ok = _.cond(cond)
 
   if not ok then
     if false_fn then
@@ -571,16 +611,12 @@ function equals:__call(x, y, invert)
   end
 end
 
----@overload fun(cond: boolean | (fun(): boolean), fmt: string, ...: string)
-claim = refself {}
-
 ---@param x any
----@param ... string
+---@param expected_types []string
 ---@return boolean?
-function claim.type(x, ...)
-  local x_type = type(x)
+function assert_type(x, expected_types)
+  local x_type = _.type(type(x))
   x_type = x_type == nil and 'nil' or x_type
-  local expected_args = { ... }
 
   for _, expected in ipairs(expected_args) do
     if x_type ~= expected then
@@ -594,26 +630,25 @@ function claim.type(x, ...)
   return false
 end
 
----@param cond (fun(): boolean) | boolean
+---@param cond Condition
 ---@param fmt string
 ---@param ... string
-function claim.when(cond, fmt, ...)
-  local cond_type = type(cond)
-  local fmt_type = type(fmt)
-  local cond_is_boolean = cond_type == 'boolean'
-  local cond_is_fun = cond_type == 'function'
-
-  if (not cond_is_boolean) and (not cond_is_fun) then
-    error(
-      'cond: Expected boolean | (fun(): boolean), got ' ..
-      (cond_type == nil and 'nil' or cond_type)
-    )
-  end
-
-  if invert(fmt_type == 'string') then
-    error(
-      'fmt: Expected string, got ' ..
-      (fmt_type == nil and 'nil' or fmt_type)
-    )
+function assert_when(cond, fmt, ...)
+  local ok = ((callable(cond)) and cond()) or cond
+  if not ok then
+    errorf(fmt, ...)
+  else
+    return true
   end
 end
+
+function assert_unless(cond, fmt, ...)
+  local use = ((callable(cond)) and not cond()) or cond
+  return assert_when(cond, fmt, ...)
+end
+
+L = literal
+when_nil = undefined
+unless_nil = defined
+unless_truthy = when_falsy
+unless_falsy = when_truthy
